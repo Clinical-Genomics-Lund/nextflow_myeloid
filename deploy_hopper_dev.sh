a079b780b50744dd2685a996345d841af6b43bfd
Viktor Henmyr <viktor_henmyr@hotmail.com>
2020-05-05 15:31:13 +0200

    first commit

---
+++ deploy_hopper_dev.sh
@@ -0,0 +1,14 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+PIPELINE_DEST="/fs1/viktor/nextflow_ovarian"
+DEST_HOST="rs-fs1.lunarc.lu.se"
+
+
+# Copy pipeline script
+scp $DIR/main.nf $DEST_HOST:$PIPELINE_DEST
+
+# Copy configuration file
+scp $DIR/configs/nextflow.hopper.config $DEST_HOST:$PIPELINE_DEST/nextflow.config
+
+# Copy other files
+scp -r $DIR/bin $DEST_HOST:$PIPELINE_DEST
